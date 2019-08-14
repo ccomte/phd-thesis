c37d39b22e491e3a54b3b12851af02e386a05ec0
Céline <celine.comte@telecom-paristech.fr>
2019-08-14 15:06:34 +0200

    Improve the readability of utils.h in Chapter 7
    Divide the constants in two categories depending on whether we need to
    modify them to run different models.

--- 7-job-scheduling/sources/utils.h
+++ 7-job-scheduling/sources/utils.h
@@ -10,12 +10,14 @@
 #include <float.h>
 
 // global constants
-#define N 2             // number of job classes
-#define S 100           // number of servers
 #define MAX 100000      // queue length
 #define R 10            // number of estimates per point
 #define WARMUP 100000   // number of warm-up steps
 #define STEADY 100000   // number of steps in steady state
+
+// constants that depend on the example
+#define N 2             // number of pools (only relevant for the toy examples)
+#define S 100           // number of servers
 
 // probability distributions
 double uniform();     // uniform on (0,1)
