3a77e368c622ec46c710181af20ee9c1d4254a0e
Céline Comte <to.celine.comte@gmail.com>
2019-09-13 13:30:18 +0200

    Add numerical results in Chapter 7

--- 7-job-scheduling/compute.sh
+++ 7-job-scheduling/compute.sh
@@ -21,59 +21,59 @@ gcc -o random-zipf-scheduling.out sources/random-zipf-scheduling.c sources/utils
 
 
 # N = 2 pools, S = 3 servers
-#./toy-bimodal-fcfs.out data/m-bimodal-0 1 1 2 0 1 2 1 2 1 5 25 1
-#./toy-bimodal-scheduling.out data/m-bimodal-1 1 1 2 0 1 2 1 2 1 5 25 1 1
-#./toy-bimodal-scheduling.out data/m-bimodal-5 1 1 2 0 1 2 1 2 1 5 25 1 5
+./toy-bimodal-fcfs.out data/m-bimodal-0 1 1 2 0 1 2 1 2 1 5 25 1
+./toy-bimodal-scheduling.out data/m-bimodal-1 1 1 2 0 1 2 1 2 1 5 25 1 1
+./toy-bimodal-scheduling.out data/m-bimodal-5 1 1 2 0 1 2 1 2 1 5 25 1 5
 
-#./toy-hyperexp-fcfs.out data/m-hyperexp-0 1 1 2 0 1 2 1 2 1 5 25 1
-#./toy-hyperexp-scheduling.out data/m-hyperexp-1 1 1 2 0 1 2 1 2 1 5 25 1 1
-#./toy-hyperexp-scheduling.out data/m-hyperexp-5 1 1 2 0 1 2 1 2 1 5 25 1 5
+./toy-hyperexp-fcfs.out data/m-hyperexp-0 1 1 2 0 1 2 1 2 1 5 25 1
+./toy-hyperexp-scheduling.out data/m-hyperexp-1 1 1 2 0 1 2 1 2 1 5 25 1 1
+./toy-hyperexp-scheduling.out data/m-hyperexp-5 1 1 2 0 1 2 1 2 1 5 25 1 5
 
-#./toy-zipf-scheduling.out data/m-zipf-1 1 1 2 0 1 2 1 2 200 2 1
-#./toy-zipf-scheduling.out data/m-zipf-5 1 1 2 0 1 2 1 2 200 2 5
-#./toy-zipf-fcfs.out data/m-zipf-0 1 1 2 0 1 2 1 2 200 2
+./toy-zipf-scheduling.out data/m-zipf-1 1 1 2 0 1 2 1 2 200 2 1
+./toy-zipf-scheduling.out data/m-zipf-5 1 1 2 0 1 2 1 2 200 2 5
+./toy-zipf-fcfs.out data/m-zipf-0 1 1 2 0 1 2 1 2 200 2
 
 
 
 # N = 2 pools, S = 2 servers
-#./toy-bimodal-fcfs.out data/n-bimodal-0 1 1 2 0 1 1 1 1 5 25 1
-#./toy-bimodal-scheduling.out data/n-bimodal-1 1 1 2 0 1 1 1 1 5 25 1 1
-#./toy-bimodal-scheduling.out data/n-bimodal-5 1 1 2 0 1 1 1 1 5 25 1 5
+./toy-bimodal-fcfs.out data/n-bimodal-0 1 1 2 0 1 1 1 1 5 25 1
+./toy-bimodal-scheduling.out data/n-bimodal-1 1 1 2 0 1 1 1 1 5 25 1 1
+./toy-bimodal-scheduling.out data/n-bimodal-5 1 1 2 0 1 1 1 1 5 25 1 5
 
-#./toy-hyperexp-fcfs.out data/n-hyperexp-0 1 1 2 0 1 1 1 1 5 25 1
-#./toy-hyperexp-scheduling.out data/n-hyperexp-1 1 1 2 0 1 1 1 1 5 25 1 1
-#./toy-hyperexp-scheduling.out data/n-hyperexp-5 1 1 2 0 1 1 1 1 5 25 1 5
+./toy-hyperexp-fcfs.out data/n-hyperexp-0 1 1 2 0 1 1 1 1 5 25 1
+./toy-hyperexp-scheduling.out data/n-hyperexp-1 1 1 2 0 1 1 1 1 5 25 1 1
+./toy-hyperexp-scheduling.out data/n-hyperexp-5 1 1 2 0 1 1 1 1 5 25 1 5
 
-#./toy-zipf-fcfs.out data/n-zipf-0 1 1 2 0 1 1 1 200 2
-#./toy-zipf-scheduling.out data/n-zipf-1 1 1 2 0 1 1 1 200 2 1
-#./toy-zipf-scheduling.out data/n-zipf-5 1 1 2 0 1 1 1 200 2 5
+./toy-zipf-fcfs.out data/n-zipf-0 1 1 2 0 1 1 1 200 2
+./toy-zipf-scheduling.out data/n-zipf-1 1 1 2 0 1 1 1 200 2 1
+./toy-zipf-scheduling.out data/n-zipf-5 1 1 2 0 1 1 1 200 2 5
 
 
 
 # S = 100 servers
-#./random-bimodal-fcfs.out data/random-2-bimodal-0 2 1 5 25 1
-#./random-bimodal-scheduling.out data/random-2-bimodal-1 2 1 5 25 1 1
-#./random-bimodal-scheduling.out data/random-2-bimodal-5 2 1 5 25 1 5
+./random-bimodal-fcfs.out data/random-2-bimodal-0 2 1 5 25 1
+./random-bimodal-scheduling.out data/random-2-bimodal-1 2 1 5 25 1 1
+./random-bimodal-scheduling.out data/random-2-bimodal-5 2 1 5 25 1 5
 
-#./random-hyperexp-fcfs.out data/random-2-hyperexp-0 2 1 5 25 1
-#./random-hyperexp-scheduling.out data/random-2-hyperexp-1 2 1 5 25 1 1
-#./random-hyperexp-scheduling.out data/random-2-hyperexp-5 2 1 5 25 1 5
+./random-hyperexp-fcfs.out data/random-2-hyperexp-0 2 1 5 25 1
+./random-hyperexp-scheduling.out data/random-2-hyperexp-1 2 1 5 25 1 1
+./random-hyperexp-scheduling.out data/random-2-hyperexp-5 2 1 5 25 1 5
 
-#./random-zipf-fcfs.out data/random-2-zipf-0 2 200 2
-#./random-zipf-scheduling.out data/random-2-zipf-1 2 200 2 1
+./random-zipf-fcfs.out data/random-2-zipf-0 2 200 2
+./random-zipf-scheduling.out data/random-2-zipf-1 2 200 2 1
 ./random-zipf-scheduling.out data/random-2-zipf-5 2 200 2 5
 
 
 
 # S = 100 servers
-#./random-bimodal-fcfs.out data/random-3-bimodal-0 3 1 5 25 1
-#./random-bimodal-scheduling.out data/random-3-bimodal-1 3 1 5 25 1 1
-#./random-bimodal-scheduling.out data/random-3-bimodal-5 3 1 5 25 1 5
+./random-bimodal-fcfs.out data/random-3-bimodal-0 3 1 5 25 1
+./random-bimodal-scheduling.out data/random-3-bimodal-1 3 1 5 25 1 1
+./random-bimodal-scheduling.out data/random-3-bimodal-5 3 1 5 25 1 5
 
-#./random-hyperexp-fcfs.out data/random-3-hyperexp-0 3 1 5 25 1
-#./random-hyperexp-scheduling.out data/random-3-hyperexp-1 3 1 5 25 1 1
-#./random-hyperexp-scheduling.out data/random-3-hyperexp-5 3 1 5 25 1 5
+./random-hyperexp-fcfs.out data/random-3-hyperexp-0 3 1 5 25 1
+./random-hyperexp-scheduling.out data/random-3-hyperexp-1 3 1 5 25 1 1
+./random-hyperexp-scheduling.out data/random-3-hyperexp-5 3 1 5 25 1 5
 
-#./random-zipf-fcfs.out data/random-3-zipf-0 3 200 2
-#./random-zipf-scheduling.out data/random-3-zipf-1 3 200 2 1
-#./random-zipf-scheduling.out data/random-3-zipf-5 3 200 2 5
+./random-zipf-fcfs.out data/random-3-zipf-0 3 200 2
+./random-zipf-scheduling.out data/random-3-zipf-1 3 200 2 1
+./random-zipf-scheduling.out data/random-3-zipf-5 3 200 2 5
